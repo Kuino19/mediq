68d5dc0da7195364c42bd79215fa3a5b259c0a31
Kuino19 <143762945+Kuino19@users.noreply.github.com>
2025-11-14 13:22:09 +0100

    Rename next.config.ts to next.config.js

---
+++ mediq/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true, // enable App Router
+  },
+  // Add any other options you had here
+};
+
+module.exports = nextConfig;
